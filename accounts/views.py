9c6c52ba9168e3724ec680edea699f186b34bbd4
Bruno Alpachino <brunolalpachino@gmail.com>
2022-09-19 23:39:02 +0300

    changed the products html page

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'accounts/home.html')
+
+
+
+def products(request):
+    return render(request, 'accounts/products.html')
+    
